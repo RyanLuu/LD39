f23091491b2fc2c617b9f3a0b457cecfa3cc1c50
RyanLuu <rl65536@gmail.com>
2017-07-29 02:37:37 -0400

    Added basic player rendering and gravity
    :penguin:

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,7 @@ class PlayState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		add(new Player(100, 100));
 	}
 
 	override public function update(elapsed:Float):Void
