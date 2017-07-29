c56dc96a50eacfdd3a443c01ff53419231f92d7f
DrWillPhd <wdmerges@gmail.com>
2017-07-29 00:47:59 -0400

    played my bois
    added player.hx

---
+++ source/Player.hx
@@ -0,0 +1,18 @@
+package;
+
+
+import flixel.FlxSprite;
+
+
+class Player extends FlxSprite{
+
+    public function new(?X:Float=0, ?Y:Float=0){
+
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    } 
+
+}
