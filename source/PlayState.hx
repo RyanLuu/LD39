87b9fc7d2acd919cf2dc18f7baa07b25d65b8eb0
johndunaske <j_dunaske@yahoo.com>
2017-07-29 15:32:44 -0400

    wth
    :b:

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,6 +2,8 @@ package;
 
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.tweens.FlxTween;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState
 {
@@ -46,13 +48,10 @@ class PlayState extends FlxState
 		FlxG.collide(player, level.platforms);
 		FlxG.overlap(player, level.grpTeleporter, playerTouchedTele);
 		FlxG.overlap(player, level.hazards, playerHitHazard);
-<<<<<<< HEAD
 		FlxG.overlap(player.drill, level.breakables, drillSmashed);
 		FlxG.collide(player, level.breakables);
 		FlxG.collide(level.platforms, level.breakables); //for moving block around
 
-=======
->>>>>>> origin/master
 	}
 
 	private function setupCamera(player:Player):Void
@@ -66,22 +65,9 @@ class PlayState extends FlxState
 		if (P.alive && P.exists)
 		{
 			remove(level.platforms);
-			remove(level.breakables);
-			remove(level.hazards);
-			level.reset();
 			level.setMap(currentMap + 1);
 			add(level.platforms);
-<<<<<<< HEAD
-			add(level.breakables);
-			add(level.hazards);
-
-			var transitionIn = new FlxSprite(320, 120, AssetPaths.transition_in__png);
-			FlxTween.tween(transitionIn, {x: transitionIn.x - 320 }, .33);
-			//add(transitionIn);
-			hud.updateHUD(">Teleporting!");
-=======
 			hud.updateHUD(">Teleported!");
->>>>>>> origin/master
 		}
 	}
 
