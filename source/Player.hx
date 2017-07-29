4bb509e5ac7ddb3b546f20ee32b1bcdef28d62b5
RyanLuu <rl65536@gmail.com>
2017-07-29 09:24:57 -0400

    More player movement

--- source/Player.hx
+++ source/Player.hx
@@ -2,34 +2,45 @@ package;
 
 
 import flixel.FlxSprite;
+import flixel.FlxObject;
 import flixel.FlxG;
 
 
 class Player extends FlxSprite
 {
+    
+    public static inline var RUN_SPEED = 80;
+    public static inline var GRAVITY = 420;
+    public static inline var JUMP_SPEED = 200;
+
     public function new(?X:Float=0, ?Y:Float=0)
     {
         super(X, Y);
         makeGraphic(10, 10);
-        acceleration.y = 420/2;
-        drag.x = 640/2;
+        drag.x = RUN_SPEED * 8;
+        acceleration.y = GRAVITY;
+        maxVelocity.x = RUN_SPEED;
+        maxVelocity.y = JUMP_SPEED;
     }
 
     override public function update(elapsed:Float):Void
     {
-        super.update(elapsed);
-
+        acceleration.x = 0;
         if(FlxG.keys.pressed.LEFT)
         {
             //facing = LEFT;
-            acceleration.x = -drag.x;
+            acceleration.x -= drag.x;
         }
         else if(FlxG.keys.pressed.RIGHT)
         {
             //facing = RIGHT;
-            acceleration.x = drag.x;
+            acceleration.x += drag.x;
         }
-
+        if(FlxG.keys.justPressed.UP)// && isTouching(FlxObject.FLOOR))
+        {
+            velocity.y = -JUMP_SPEED;
+        }
+        super.update(elapsed);
     }
 
 }
