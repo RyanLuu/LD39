105aa5be37c15020003e3e878b0df4541cceb6f5
johndunaske <j_dunaske@yahoo.com>
2017-07-29 09:54:20 -0400

    Teleportation Failure.
    Unknown errors!

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,6 +5,7 @@ import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.tile.FlxTilemap;
 import flixel.FlxObject;
 import flixel.FlxG;
+import flixel.group.FlxGroup;
 
 
 class PlayState extends FlxState
@@ -15,20 +16,25 @@ class PlayState extends FlxState
 
 	private var map:FlxOgmoLoader;
 	private var platforms:FlxTilemap;
+	private var grpTeleporter:FlxGroup;
+	private var grpMaps = [AssetPaths.TestingGrounds__oel, AssetPaths.TeleTesting__oel];
+	private var currentMap = 0;
 
 
 	override public function create():Void
 	{
 		super.create();
 
-		player = new Player();
-		hud = new HUD();
-
 		map = new FlxOgmoLoader(AssetPaths.TestingGrounds__oel);
 		platforms = map.loadTilemap(AssetPaths.tiles__png, 16, 16, "platforms");
 		platforms.setTileProperties(0, FlxObject.ANY);
 		platforms.setTileProperties(1, FlxObject.NONE);
-		
+
+		map.loadEntities(placeEntities, "entities");
+
+		player = new Player();
+		hud = new HUD();
+
 		add(platforms);
 		add(player);
 
@@ -41,11 +47,39 @@ class PlayState extends FlxState
 		super.update(elapsed);
 
 		FlxG.collide(player, platforms);
+		FlxG.overlap(player, grpTeleporter, playerTouchedTele);
 
 	}
 
 	public function setupCamera(player:Player):Void
 	{
 		FlxG.camera.follow(player, TOPDOWN, 1);
+	}
+
+	private function placeEntities(entityName:String, entityData:Xml)
+	{
+		var x:Float = Std.parseInt(entityData.get("x"));
+		var y:Float = Std.parseInt(entityData.get("y"));
+
+		if (entityName == "player")
+		{
+			player.x = x;
+			player.y - y;
+		}
+
+		if (entityName == "teleport")
+		{
+			grpTeleporter.add(new Teleportation(x,y));
+		}
+	}
+	private function playerTouchedTele(P:Player, T:Teleportation):Void
+	{
+		if (P.alive && P.exists)
+		{
+			map = new FlxOgmoLoader(grpMaps[currentMap + 1]);
+			platforms = map.loadTilemap(AssetPaths.tiles__png, 16, 16, "platforms");
+			platforms.setTileProperties(0, FlxObject.ANY);
+			platforms.setTileProperties(1, FlxObject.NONE);
+		}
 	}
 }
