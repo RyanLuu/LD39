1ab321b9d79f43daecac0d2b810284f7a910458c
DrWillPhd <wdmerges@gmail.com>
2017-07-31 00:44:57 -0400

    More boi fix

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -211,6 +211,10 @@ class PlayState extends FlxState
 				}
 				case "disableBoi": {
 					hud.updateHUD("WARNING: Disabling B.O.I. to conserve power.");
+					player.boi.mode = 3;
+				}
+				case "disableDrill": {
+					hud.updateHUD("WARNING: Disabling drill functionality to conserve power.");
 					player.boi.mode = 2;
 				}
 				default: "Invalid event triggered.";
